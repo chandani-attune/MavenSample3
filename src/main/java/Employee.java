dad759224b09428669153807251ab9c64df4162c
chandani.thacker <chandani.thacker@attuneww.com>
2016-05-12 16:03:11 +0530

    init setup

---
+++ src/main/java/Employee.java
@@ -0,0 +1,22 @@
+
+public class Employee {
+	public int getSalary(int numofDays,int dailySalary)
+	{
+		return numofDays*dailySalary;
+	}
+	
+	
+	public void newMethods()
+	{
+		System.out.println("Simple method");
+		
+	}
+	
+	
+	public void newMethods2()
+	{
+		System.out.println("Simple method  2");
+		
+	}
+
+}
---
+++ src/main/java/Student.java
@@ -0,0 +1,25 @@
+
+public class Student 
+{
+	public int countAvg(int m1,int m2,int m3 )
+	{
+		return((m1+m2+m3)/3);
+	}
+	
+	public String getGrades(double avgMarks)
+	{
+		if(avgMarks>=90)
+			return "A";
+		else if(avgMarks>=70 & avgMarks<90)
+			return "B";
+		else
+			return "C";
+	}
+
+	public void newMethods3()
+	{
+		System.out.println("Simple method  3");
+		
+	}
+
+}
